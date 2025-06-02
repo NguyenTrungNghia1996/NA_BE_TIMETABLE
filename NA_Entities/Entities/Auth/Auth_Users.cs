2ec7ef2dc9875eafa1a95750a2691ff99f77f108
hatd <tranduyhakma@gmail.com>
2025-06-02 10:43:06 +0700

    commit

--- NA_Entities/Entities/Auth/Auth_Users.cs
+++ NA_Entities/Entities/Auth/Auth_Users.cs
@@ -12,18 +12,16 @@ namespace NA_Entities.Entities.Auth
         [Key]
         public int Id { get; set; } = 0;
         [Required(ErrorMessage = "Tên đăng nhập không được để trống")]
-        [StringLength(100, ErrorMessage = "Tối đa 100 ký tự")]
+        [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         [Required(ErrorMessage = "Tên đầy đủ không được để trống")]
-        [StringLength(100, ErrorMessage = "Tối đa 100 ký tự")]
+        [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
         public string Hoten { get; set; } = string.Empty;
-        public DateTime? Ngaysinh { get; set; }
-        public int? Id_Gioitinh { get; set; }
         [Required(ErrorMessage = "Vui lòng chọn đơn vị")]
         public int Id_Donvi { get; set; }
-        public int? Id_Chucvu { get; set; }
-        public bool IsActive { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsAdmin { get; set; }
     }
     public class Auth_Users_List
     {
@@ -31,13 +29,9 @@ namespace NA_Entities.Entities.Auth
         public int Id { get; set; } = 0;
         public string Username { get; set; } = string.Empty;
         public string Hoten { get; set; } = string.Empty;
-        public DateTime? Ngaysinh { get; set; }
-        public int? Id_Gioitinh { get; set; }
-        public string Tengioitinh { get; set; } = string.Empty;
         public int Id_Donvi { get; set; }
-        public string Tendonvi { get; set; } = string.Empty;
-        public int? Id_Chucvu { get; set; }
-        public string Tenchucvu { get; set; } = string.Empty;
-        public bool IsActive { get; set; }
+        public string? Tendonvi { get; set; } = string.Empty;
+        public bool? IsActive { get; set; }
+        public bool? IsAdmin { get; set; }
     }
 }
