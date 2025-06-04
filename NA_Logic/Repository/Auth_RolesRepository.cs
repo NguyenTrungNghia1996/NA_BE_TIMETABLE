b34d1ebe4214b2a7a30fdb9ed4dc7301903b49b9
hatd <tranduyhakma@gmail.com>
2025-06-04 14:21:06 +0700

    commit

---
+++ NA_Logic/Repository/Auth_RolesRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NA_Logic.IRepository;
+
+namespace NA_Logic.Repository
+{
+    public class Auth_RolesRepository : IAuth_RolesRepository
+    {
+        
+    }
+}
