472284460f8caa55407289a947278127f236322b
hatd <tranduyhakma@gmail.com>
2025-06-05 15:37:57 +0700

    commit

--- NA_Entities/Entities/Auth/Auth_Menus.cs
+++ NA_Entities/Entities/Auth/Auth_Menus.cs
@@ -14,22 +14,22 @@ namespace NA_Entities.Entities.Auth
         public string Title { get; set; } = string.Empty;
         [Required(ErrorMessage = "Key không được để trống")]
         [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
-        public string Key { get; set; } = string.Empty;
+        public string? Key { get; set; } = string.Empty;
         public string? Url { get; set; } = string.Empty;
         public string? Icon { get; set; } = string.Empty;
         public int? Parent_Id { get; set; }
         [Required(ErrorMessage = "Vị trí bit không được để trống")]
-        public int PermissionBit { get; set; } = 0;
+        public int? PermissionBit { get; set; } = 0;
     }
     public class Auth_MenusList
     {
         public int? Stt { get; set; } = 0;
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Key { get; set; } = string.Empty;
+        public string? Key { get; set; } = string.Empty;
         public string? Url { get; set; } = string.Empty;
         public string? Icon { get; set; } = string.Empty;
         public int? Parent_Id { get; set; }
-        public int PermissionBit { get; set; } = 0;
+        public int? PermissionBit { get; set; } = 0;
     }
 }
