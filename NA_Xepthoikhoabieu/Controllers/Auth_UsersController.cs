5102e1a93727f2531d61465e3185725399bd8b04
loannk <loannk82@gmail.com>
2025-06-06 10:46:27 +0700

    update getlist user

--- NA_Xepthoikhoabieu/Controllers/Auth_UsersController.cs
+++ NA_Xepthoikhoabieu/Controllers/Auth_UsersController.cs
@@ -49,8 +49,6 @@ namespace NA_Xepthoikhoabieu.Controllers
             {
                 return ApiResult.Forbidden("Không có quyền truy cập, vui lòng liên hệ admin");
             }
-            if (PageIndex < 1 || PageSize < 1)
-                return ApiResult.BadRequest($"PageIndex hoặc PageSize không hợp lệ, vui lòng kiểm tra lại (PageIndex >= 1; PageSize >= 1)");
             int totalrecord = 0;
             var list = _auth.GetListUsers_Paging(PageIndex, PageSize, search, ref totalrecord);
             return ApiResult.Success(new
