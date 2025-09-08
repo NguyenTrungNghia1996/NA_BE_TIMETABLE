1ef3f54b83362a79824b46b54b3a22466d3ca412
loanhtt <loannk82@gmail.com>
2025-09-08 11:41:41 +0700

    up

--- NA_Entities/Entities/Dtos/DM_DonviDto.cs
+++ NA_Entities/Entities/Dtos/DM_DonviDto.cs
@@ -12,7 +12,6 @@ namespace NA_Entities.Entities.Dtos
         public int Id { get; set; } = 0;
         [Required(ErrorMessage = "Tên đơn vị không được để trống")]
         [StringLength(100, ErrorMessage = "Tối đa 100 ký tự")]
-        [MinLength(20, ErrorMessage = "Ít nhất 20 ký tự")]
         public string TenDonvi { get; set; } = string.Empty;
         [Required(ErrorMessage = "Địa chỉ không được để trống")]
         [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
@@ -43,7 +42,7 @@ namespace NA_Entities.Entities.Dtos
     {
         public int Id { get; set; } = 0;
         [Required(ErrorMessage = "Tên đơn vị không được để trống")]
-        [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
+        [StringLength(100, ErrorMessage = "Tối đa 100 ký tự")]
         public string TenDonvi { get; set; } = string.Empty;
         [Required(ErrorMessage = "Địa chỉ không được để trống")]
         [StringLength(200, ErrorMessage = "Tối đa 200 ký tự")]
@@ -65,7 +64,7 @@ namespace NA_Entities.Entities.Dtos
     }
     public class Donvi_NgayDto
     {
-        public int Id { get; set; }= 0;
+        public int Id { get; set; } = 0;
         public List<int> Id_Ngay { get; set; } = new List<int>();
 
     }
