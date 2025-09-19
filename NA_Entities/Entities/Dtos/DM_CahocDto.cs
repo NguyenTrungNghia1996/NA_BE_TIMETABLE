02178d1a07faaf3bbb3d3c1e10cd587f7a9949ab
loanhtt <loannk82@gmail.com>
2025-09-19 09:42:35 +0700

    up

--- NA_Entities/Entities/Dtos/DM_CahocDto.cs
+++ NA_Entities/Entities/Dtos/DM_CahocDto.cs
@@ -34,10 +34,10 @@ namespace NA_Entities.Entities.Dtos
         public int Id_ca { get; set; }
         public string Ten_ca { get; set; }
         [RegularExpression(@"^-?\d+$", ErrorMessage = "Không được nhập số thập phân")]
-        [Range(0, 20, ErrorMessage = "Số tiết phải là số nguyên dương và nhỏ hơn 20")]
+        [Range(0, 99, ErrorMessage = "Số tiết phải là số nguyên dương và nhỏ hơn 100")]
         public double? So_tiet { get; set; } = 0;
         [RegularExpression(@"^-?\d+$", ErrorMessage = "Không được nhập số thập phân")]
-        [Range(0, 20, ErrorMessage = "Số nhóm phải là số nguyên dương và nhỏ hơn 20")]
+        [Range(0, 99, ErrorMessage = "Số nhóm phải là số nguyên dương và nhỏ hơn 100")]
         public double? So_nhom { get; set; } = 0;
     }
 
