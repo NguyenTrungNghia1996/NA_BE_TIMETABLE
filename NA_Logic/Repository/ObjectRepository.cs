ae63a758439c37dd19d19aaf4af69e223900a0e7
loanhtt <loannk82@gmail.com>
2025-09-23 09:32:28 +0700

    update

--- NA_Logic/Repository/ObjectRepository.cs
+++ NA_Logic/Repository/ObjectRepository.cs
@@ -2608,7 +2608,7 @@ namespace NA_Logic.Repository
                 var cacTietCuaLop = _dsTietGoc.Where(t => t.Id_lop == idLop && t.Ngay == 0 && t.Tiet == 0).ToList();
                     foreach (var tietGoc in cacTietCuaLop)
                     {
-                        TimViTriXepDuoc_Lop(tietGoc, idDonvi);
+                        TimViTriXepDuoc_TietChuaXep(tietGoc, idDonvi);
                         if (tietGoc.Ds_vi_tri_xep_duoc != null && tietGoc.Ds_vi_tri_xep_duoc.Count > 0)
                         {
                             bool coViTriTrung = tietGoc.Ds_vi_tri_xep_duoc.Any(viTri => viTri.Ca == idCa &&
@@ -2925,7 +2925,7 @@ namespace NA_Logic.Repository
                 var cacTietCuaGV = _dsTietGoc.Where(t => t.Id_giao_vien == idGV && t.Ngay == 0 && t.Tiet == 0).ToList();
                 foreach (var tietGoc in cacTietCuaGV)
                 {
-                    TimViTriXepDuoc_Lop(tietGoc, idDonvi);
+                    TimViTriXepDuoc_TietChuaXep(tietGoc, idDonvi);
                     if (tietGoc.Ds_vi_tri_xep_duoc != null && tietGoc.Ds_vi_tri_xep_duoc.Count > 0)
                     {
                         bool coViTriTrung = tietGoc.Ds_vi_tri_xep_duoc.Any(viTri => viTri.Ca == idCa &&
