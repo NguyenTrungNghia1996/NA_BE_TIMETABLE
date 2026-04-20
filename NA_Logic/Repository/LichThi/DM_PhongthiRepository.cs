bff7ff1311f1463cc0fbafb59dc1718b7540908f
loanhtt <loannk82@gmail.com>
2026-04-20 11:21:16 +0700

    update

--- NA_Logic/Repository/LichThi/DM_PhongthiRepository.cs
+++ NA_Logic/Repository/LichThi/DM_PhongthiRepository.cs
@@ -153,8 +153,7 @@ namespace NA_Logic.Repository
             try
             {
 
-                return _dbContext.Database.SqlQuery<int>($@"
-                          select 1 from Phongthi_Thisinh where Id_phong = {Id}").Any();
+                return _dbContext.Phongthi_Thisinh.Any(c => c.Id_phong == Id);
             }
             catch (Exception)
             {
--- NA_Logic/Repository/LichThi/DM_ThisinhRepository.cs
+++ NA_Logic/Repository/LichThi/DM_ThisinhRepository.cs
@@ -218,8 +218,7 @@ namespace NA_Logic.Repository
             try
             {
 
-                return _dbContext.Database.SqlQuery<int>($@"
-                          select 1 from Phongthi_Thisinh where Id_thi_sinh = {Id}").Any();
+                return _dbContext.Phongthi_Thisinh.Any(c => c.Id_thi_sinh == Id);
             }
             catch (Exception)
             {
